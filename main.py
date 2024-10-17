1c1495660f03831d26ea905eb0da51209237db9f
gosha1337 <90519734+g0sha1337@users.noreply.github.com>
2024-10-17 15:48:10 +0300

    fix checksum

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ def ask_card_sute(number_of_card):
     return bin(suite)[2::].zfill(2)# fill 2 bit if they are empty
 
 def ask_card_rank(number_of_card):
-    answer = 0
-    while (!(1<=answer<=9)):
-        answer = int(input(f'What is the card rank of the {number_of_card}\'s card?\n\n1 - Six (Шестерка)\n2 - Seven (Семерка)\n3 - Eight (Восьмерка)\n4 - Nine (Девятка)\n5 - Ten (Десятка)\n6 - Jack (Валет)\n7 - Queen (Дама)\n8 - King (Король)\n9 - Ace (Туз)'))
+    # !TODO create validation of input
+    rank = int(input(f'What is the card rank of the {number_of_card}\'s card?\n\n1 - Six (Шестерка)\n2 - Seven (Семерка)\n3 - Eight (Восьмерка)\n4 - Nine (Девятка)\n5 - Ten (Десятка)\n6 - Jack (Валет)\n7 - Queen (Дама)\n8 - King (Король)\n9 - Ace (Туз)'))
     return bin(rank)[2::].zfill(4) # fill 4 bit if they are empty
 
 def bits2seed(bits):
@@ -30,22 +29,35 @@ def bits2seed(bits):
         exit(-1)
     return seed
 
+def checksum (bits):
+    int_value = int(bits, 2)
+
+    # Преобразуем целое число в байты (16 байт для 128 бит)
+    byte_value = int_value.to_bytes(16, byteorder='big')
+
+    # Получаем SHA-256 хэш
+    hash_value = hashlib.sha256(byte_value).digest()  # Используем .digest() для получения байтов
+
+    # Преобразуем байты хэша в двоичный формат
+    binary_hash = ''.join(format(byte, '08b') for byte in hash_value)
+
+    return binary_hash[:4]  # Возвращаем первые 4 бита
+
 if __name__ == '__main__':
     print("Deck of 36 card to 12 words bip39 seed")
     value128bit = ""
-    #value128bit += '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    value128bit += '10100000000010000001010000001001000000001001110001000000010010000000001000000100010000000001000100100100101000010000000011100100'
 
-    for i in range(0,23): #main cycle to read combination of 22 cardz
-        carddata = ""
-        carddata +=ask_card_sute(i+1)
-        carddata +=ask_card_rank(i+1)
-        print(f'your num is {carddata}')
-        value128bit+=carddata
+    # for i in range(0,23): #main cycle to read combination of 22 cardz
+    #     carddata = ""
+    #     carddata +=ask_card_sute(i+1)
+    #     carddata +=ask_card_rank(i+1)
+    #     print(f'your num is {carddata}')
+    #     value128bit+=carddata
 
     # value128bit = value128bit[:-4] # cut last 4 bit from 22's card to make 128bit value
-    hash_value = hashlib.sha256(value128bit.encode()).digest()
-    binary_hash = ''.join(format(byte, '08b') for byte in hash_value)
-    value128bit += binary_hash[:4]
+    
+    value128bit += checksum(value128bit)
     print(f'{value128bit} - {len(value128bit)} bits')
     seed = bits2seed(value128bit)
     print (f'COOL, YOUR SEED IS {seed}')
